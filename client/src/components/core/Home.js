d4ee81903dd0f3095de8c2edbf398ae5419a3809
leanny01 <dleannykalangu@gmail.com>
2021-01-20 18:48:02 +0200

    Loaded fake data

--- client/src/components/core/Home.js
+++ client/src/components/core/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Messages} from './../messages'
+
 
 const Home = ()=>{
+
+    
     return(
+        <div>
         <div>Hi there this is a work in progress, keep watching the space...</div>
+        <Messages/>
+        </div>
+        
     )
 }
 
---
+++ client/src/components/messages/index.js
@@ -0,0 +1,9 @@
+import Messages from './MessageList'
+// import EditMessage from './UpdateMessage'
+// import DeleteMessage from './DeleteMessage'
+// import ViewMessage from './ViewMessage'
+
+
+
+
+export {Messages};
